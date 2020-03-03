c1e1290a1f97a1b688cb7913ec20b737a9d785ff
isaacRodgz <chack.rb@gmail.com>
2020-03-03 14:56:41 -0600

    ADD excercise 4 and conclusion

--- kaggle.py
+++ kaggle.py
@@ -10,7 +10,154 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import GridSearchCV
 import numpy as np
 import pandas as pd
+import nltk
 
+def sortFreqDict(freqdict):
+    aux = [(freqdict[key], key) for key in freqdict]
+    aux.sort()
+    aux.reverse()
+    return aux
+
+def emo_vect_tfidf_norm(tweets, emotions_dict):
+
+    words_emo = []
+
+    for tweet in tweets:
+
+        tweet_vect = np.zeros(len(emotions_dict.iloc[0]))
+
+        # Transforma cada tweet a su vector de emociones
+        for word in tweet.split():
+
+            # Se verifica si la palabra está incluida en el diccionario de emociones
+            try:
+                if len(emotions_dict.loc[word]) == 1:
+                    w_vect = np.array(emotions_dict.loc[word])
+                else:
+                    w_vect = np.array(emotions_dict.loc[word].sum(axis = 0, skipna = True))
+                tweet_vect += np.array(w_vect)
+            except:
+                pass
+
+        words_emo.append(tweet_vect)
+
+    words_emo = np.array(words_emo)
+
+    for i in range(words_emo.shape[0]):
+        emotions_index = np.nonzero(words_emo[i])[0]
+        for j in emotions_index:
+            num_tweets = len(np.nonzero(words_emo[:, j])[0])
+            words_emo[i][j] = words_emo[i][j]*np.log10((words_emo.shape[0]/num_tweets) + 1)
+
+    for i in range(words_emo.shape[0]):
+        norm = np.linalg.norm(words_emo[i])
+
+        if norm == 0:
+            norm = 1
+
+        words_emo[i] = words_emo[i]/norm
+
+    return words_emo
+
+def emo_vect_sel_tfidf_norm(tweets, emotions_dict):
+
+    words_emo = []
+    emo_list = list(sel_emotions_dict.Categoría.unique())
+    num_cols = len(emo_list)
+
+    for tweet in tweets:
+
+        tweet_vect = np.zeros(len(emotions_dict.iloc[0]))
+
+        # Transforma cada tweet a su vector de emociones
+        for word in tweet.split():
+
+             # Se verifica si la palabra está incluida en el diccionario de emociones
+            try:
+                if len(emotions_dict.loc[word]) == 1:
+                    w_vect = np.array(sel_emotions_dict.loc[word][:-1], dtype=np.float)
+                else:
+                    w_vect = np.array(emotions_dict.loc[word][:-1].sum(axis = 0, skipna = True), dtype=np.float)
+
+                tweet_vect += w_vect
+            except:
+                pass
+
+        words_emo.append(tweet_vect)
+
+    words_emo = np.array(words_emo)
+
+    for i in range(words_emo.shape[0]):
+        emotions_index = np.nonzero(words_emo[i])[0]
+        for j in emotions_index:
+            num_tweets = len(np.nonzero(words_emo[:, j])[0])
+            words_emo[i][j] = words_emo[i][j]*np.log10((words_emo.shape[0]/num_tweets) + 1)
+
+    for i in range(words_emo.shape[0]):
+        norm = np.linalg.norm(words_emo[i])
+
+        if norm == 0:
+            norm = 1
+
+        words_emo[i] = words_emo[i]/norm
+
+    return words_emo
+
+def bow2(tr_txt, V, dict_indices):
+    BOW = np.zeros((len(tr_txt),len(V)), dtype=int)
+    cont_doc = 0
+    for tr in tr_txt:
+        fdist_doc = nltk.FreqDist(tr.split())
+        for word in fdist_doc:
+            if word in dict_indices:
+                BOW[cont_doc, dict_indices[word]] = fdist_doc[word]
+        cont_doc += 1
+
+    return BOW
+
+def bow5(tr_txt, V, dict_indices):
+
+    BOW = np.zeros((len(tr_txt),len(V)), dtype=np.float64)
+    cont_doc = 0
+
+    df = {}
+
+    for word in dict_indices.keys():
+        for tr in tr_txt:
+            if word in tr:
+                if not word in df:
+                    df[word] = 1
+                else:
+                    df[word] += 1
+
+    for tr in tr_txt:
+        fdist_doc = nltk.FreqDist(tr.split())
+        for word in fdist_doc:
+            if word in dict_indices:
+                BOW[cont_doc, dict_indices[word]] = fdist_doc[word]*np.log10((len(tr_txt)/df[word]))
+        cont_doc += 1
+
+
+    for i in range(BOW.shape[0]):
+        norm = np.linalg.norm(BOW[i])
+
+        if norm == 0:
+            norm = 1
+
+        BOW[i] = BOW[i]/norm
+
+    return BOW
+
+
+# Recurso de emociones CANADA
+emotions_dict = pd.read_csv("emolex.csv")
+emotions_dict = emotions_dict.set_index('Spanish (es)')
+
+# Recurso de emociones SEL
+sel_emotions_dict = pd.read_csv("SEL_full.txt", sep='\t', encoding = "ISO-8859-1")
+sel_emotions_dict = sel_emotions_dict.set_index('Palabra')
+
+# Lee corpus de tweets
 reader = CategorizedPlaintextCorpusReader('./', r'mex.*\.txt', cat_pattern=r'(\w+)/*')
 
 tweets_train = reader.raw('mex_train.txt').split('\n')[:-1]
@@ -23,33 +170,51 @@ labels_val = list(map(int, labels_val))
 
 tweets_test = reader.raw('mex_test.txt').split('\n')[:-1]
 
-#vectorizer = CountVectorizer(min_df=3, max_df=500, ngram_range=(1, 4), analyzer='word')
+corpus_palabras = []
+for doc in tweets_train:
+    corpus_palabras += doc.split()
+fdist = nltk.FreqDist(corpus_palabras)
 
-vectorizer = TfidfVectorizer(min_df=3, max_df=1000, stop_words='spanish', ngram_range=(4, 8), analyzer='char')
+V = sortFreqDict(fdist)
+V = V[:5000]
 
-vtf = vectorizer.fit_transform(tweets_train)
+dict_indices = dict()
+cont = 0
+for weight, word in V:
+    dict_indices[word] = cont
+    cont += 1
 
-print(vtf.shape)
+parameters = {'C': [.05, .12, .25, .5, 1, 2, 4]}
 
-#pca = PCA(n_components=20000, svd_solver='auto')
-#X = pca.fit_transform(vtf.toarray())
-#print(X.transpose().shape)
-
-parameters = {'C': [0.007, 0.01, 0.03, .05, .12, .25, .5, 1, 2, 4, 8]}
-
-svr = svm.LinearSVC(class_weight='balanced', max_iter=1500)
+svr = svm.LinearSVC(class_weight='balanced')
 grid = GridSearchCV(estimator=svr, param_grid=parameters, n_jobs=8, scoring="f1_macro", cv=5)
 
-grid.fit(vtf, labels_train)
+BOW_train_trad=bow2(tweets_train, V, dict_indices)
+BOW_val_trad=bow2(tweets_val, V, dict_indices)
 
-labels_pred = grid.predict(vectorizer.transform(tweets_val))
+BOW_train_emo=emo_vect_tfidf_norm(tweets_train, emotions_dict)
+BOW_val_emo=emo_vect_tfidf_norm(tweets_val, emotions_dict)
+
+BOW_train_emo_sel=emo_vect_sel_tfidf_norm(tweets_train, sel_emotions_dict)
+BOW_val_emo_sel=emo_vect_sel_tfidf_norm(tweets_val, sel_emotions_dict)
+
+BOW_train = np.concatenate((BOW_train_trad, BOW_train_emo, BOW_train_emo_sel), axis=1)
+BOW_val = np.concatenate((BOW_val_trad, BOW_val_emo, BOW_val_emo_sel), axis=1)
+
+grid.fit(BOW_train, labels_train)
+
+labels_pred = grid.predict(BOW_val)
 
 p, r, f, _ = precision_recall_fscore_support(labels_val, labels_pred, average='macro', pos_label=None)
 
-print(p, r, f)
 print(confusion_matrix(labels_val, labels_pred))
 print(metrics.classification_report(labels_val, labels_pred))
 
-pred_test = grid.predict(vectorizer.transform(tweets_test))
+BOW_test_trad=bow5(tweets_test, V, dict_indices)
+BOW_test_emo=emo_vect_tfidf_norm(tweets_test, emotions_dict)
+BOW_test_emo_sel=emo_vect_sel_tfidf_norm(tweets_test, sel_emotions_dict)
+BOW_test=np.concatenate((BOW_test_trad, BOW_test_emo, BOW_test_emo_sel), axis=1)
+
+pred_test = grid.predict(BOW_test)
 result = pd.DataFrame(data=pred_test, index=np.array(range(len(pred_test))), columns=["Expected"])
 result.to_csv("result.csv", index=True)
