dd77669a4b347245d67c9eee3d342c88c24b221a
IsaacRodgz <chack.rb@gmail.com>
2020-04-11 23:22:55 -0500

    ADD Mex-A3T baseline

--- tarea3/tarea3.py
+++ tarea3/tarea3.py
@@ -1,62 +1,66 @@
-from sklearn.feature_extraction.text import CountVectorizer
-from nltk.tokenize import TweetTokenizer
-from nltk.corpus import stopwords
-from nltk import FreqDist
-from nltk import bigrams
-import preprocessor as p
+import numpy as np
+from sklearn.metrics import accuracy_score, confusion_matrix, f1_score, precision_recall_fscore_support, roc_auc_score
+from sklearn import metrics, preprocessing
+from sklearn.model_selection import GridSearchCV
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from sklearn.svm import SVC
 
-with open('mex_train.txt', 'r') as f:
-    corpus = f.readlines()
 
-punct = set(['.', ',', ';', ':', '-', '!', '¡', '¿', '?', '"', '\'', '...', '<url>', '*', '@usuario'])
+def read_data():
 
-def process_word(w):
-    is_punct = True if w in punct else False
-    is_digit = w.isnumeric()
-    is_stopword = w in stopwords.words('spanish')
+    # Training set
+    with open('mex_train.txt', 'r') as f:
+        corpus_train = f.readlines()
+    with open('mex_train_labels.txt', 'r') as f:
+        labels_train = f.readlines()
+    labels_train = [int(lab.strip('\n')) for lab in labels_train]
+    tweets_train = [tw.strip('\n') for tw in corpus_train]
 
-    return "" if is_punct or is_digit or is_stopword else w.lower()
+    # Validation set
+    with open('mex_val.txt', 'r') as f:
+        corpus_val = f.readlines()
+    with open('mex_val_labels.txt', 'r') as f:
+        labels_val = f.readlines()
+    labels_val = [int(lab.strip('\n')) for lab in labels_val]
+    tweets_val = [tw.strip('\n') for tw in corpus_val]
 
-def process_sentence(sent):
-    s = []
-    for w in sent:
+    # Test set
+    with open('mex_test.txt', 'r') as f:
+        corpus_test = f.readlines()
+    with open('mex_test_labels.txt', 'r') as f:
+        labels_test = f.readlines()
+    labels_test = [int(lab.strip('\n')) for lab in labels_test]
+    tweets_test = [tw.strip('\n') for tw in corpus_test]
 
-        is_punct = True if w in punct else False
-        is_digit = w.isnumeric()
-        is_stopword = w in stopwords.words('spanish')
+    return tweets_train, labels_train, tweets_val, labels_val, tweets_test, labels_test
 
-        if not(is_punct or is_digit or is_stopword):
-            s.append(w.lower())
 
-    return " ".join(s)
+if __name__ == '__main__':
 
-tk = TweetTokenizer()
+    X_train, y_train, X_val, y_val, X_test, y_test = read_data()
 
-tokens = [process_word(w) for sent in corpus for w in tk.tokenize(sent)]
-tokens = list(filter(None, tokens))
-dist = FreqDist(tokens)
+    print("{0} for training\n{1} for validation\n{2} for test\n".format(len(X_train), len(X_val), len(X_test)))
 
-"""
-sentences = [process_sentence(sent) for sent in corpus for w in tk.tokenize(sent)]
-vectorizer = CountVectorizer()
-X = vectorizer.fit_transform(sentences)
-print(vectorizer.get_feature_names()[:50])
-"""
+    vectorizer = CountVectorizer(analyzer = 'word', ngram_range = (2, 2), min_df = 1, max_df = 2000)
+    X_train = vectorizer.fit_transform(X_train)
+    X_val = vectorizer.transform(X_val)
+    X_test = vectorizer.transform(X_test)
 
-# Unigrams
+    parameters = {'C': [2, 2.5, 3, 3.5, 4], 'gamma': [.05, .12, .25, .5, 2]}
 
-total_words = len(tokens)
+    svr = SVC()
 
-unigrams = {}
+    grid = GridSearchCV(estimator=svr, param_grid=parameters, n_jobs=8, scoring="f1_macro", cv=5, verbose=3)
 
-for w in dist.keys():
-    unigrams[w] = float(tokens.count(w))/total_words
+    grid.fit(X_train, y_train)
 
-common = [(k,v) for k, v in sorted(unigrams.items(), key=lambda item: item[1], reverse=True)]
-#print(common[:20])
+    y_pred = grid.predict(X_val)
 
-# Bigrams
+    p, r, f, _ = precision_recall_fscore_support(y_val, y_pred, average='macro', pos_label=None)
 
-sentences = [process_sentence(sent) for sent in corpus for w in tk.tokenize(sent)]
-bigrams_w = list(bigrams(sentences))
-print(bigrams_w[:10])
+    print(metrics.classification_report(y_val, y_pred))
+
+    print(grid.cv_results_)
+
+    #y_pred_test = grid.predict(X_test)
+    #print(metrics.classification_report(y_test, y_pred_test))
